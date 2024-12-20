4c02a4a2ba0b7003d3f28dbbd30d52a18e764d5b
Truong <26A4041674@hvnh.edu.vn>
2024-12-20 08:27:22 +0700

    Them cac ham structs

--- main.cpp
+++ main.cpp
@@ -29,3 +29,53 @@ void hieuung(const char *text)
     }
     putchar('\n');
 }
+// Cac ham khai bao cac struct
+typedef struct TKMK
+{
+    char tk[50];
+    char mk[50];
+    int role;
+    struct TKMK *next;
+} TKMK;
+
+typedef struct Thue
+{
+    char MaTro[15];
+    char HoTenKT[30];
+    char Diachitro[100];
+    char SDTKT[20];
+    int Dien;
+    int Nuoc;
+    int DichVu;
+    int TienPhong;
+    int TongTien;
+    char TrangThai[20];
+    struct Thue *next;
+} Thue;
+
+typedef struct Trong
+{
+    char MaTro[15];
+    char HoTenCT[30];
+    char Diachitro[100];
+    char SDTCT[20];
+    int DienSo;
+    int NuocSo;
+    int DichVuThang;
+    int TienPhong;
+    char TrangThai[20];
+    struct Trong *next;
+} Trong;
+
+typedef struct
+{
+    char MaTro[15];
+    char HoTenKT[30];
+    char SDTKT[20];
+    char PhanHoi[200];
+} PhanHoi;
+typedef struct
+{
+    char NoiDung[200];
+    char Ngay[20];
+} ThongBao;
